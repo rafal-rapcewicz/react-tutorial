37e522556df596ca77ac54f6ca95305b30337323
Rafal Rapcewicz <r.rapcewicz@papers.ch>
2020-10-15 10:43:51 +0200

    rdy without history

--- src/App.tsx
+++ src/App.tsx
@@ -17,21 +17,67 @@ const Square = ({
 
 const Board = () => {
   interface BoardState {
-    status: string;
+    status: string | null;
+    winner: string | null;
     squares: string[];
+    xIsNext: boolean;
   }
-  const [state, setState] = React.useState<BoardState>({
-    status: "Next player: X",
-    squares: Array(9).fill(null),
-  });
-  const handleClick = (i: number) => {
-    const squares = state.squares.slice();
-    squares[i] = "X";
-    setState({ ...state, squares });
+  const xIsNext = true;
+  const squares = Array(9).fill(null);
+  const nextPlayer = (xIsNext: boolean): string => (xIsNext ? "X" : "O");
+  const calculateWinner = (squares: string[]): string | null => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    const completed = lines.filter(
+      ([a, b, c]) =>
+        squares[a] && squares[a] === squares[b] && squares[a] === squares[c]
+    );
+
+    return completed.length > 0 ? squares[completed[0][0]] : null;
   };
+  const getStatus = (squares: string[], xIsNext: boolean): string => {
+    const winner = calculateWinner(squares);
+
+    return winner ? `Winner: ${winner}` : `Next player: ${nextPlayer(xIsNext)}`;
+  };
+  const [state, setState] = React.useState<BoardState>({
+    status: getStatus(squares, xIsNext),
+    winner: null,
+    squares,
+    xIsNext,
+  });
+  
+
+  const handleClick = (i: number) => {
+    if (state.winner || state.squares[i]) {
+      return;
+    }
+
+    const squares = state.squares.slice();
+    squares[i] = nextPlayer(state.xIsNext);
+    setState({
+      ...state,
+      squares,
+      xIsNext: !state.xIsNext,
+      winner: calculateWinner(squares),
+      status: getStatus(squares, state.xIsNext),
+    });
+  };
+
   const renderSquare = (i: number) => (
     <Square value={state.squares[i]} onClick={() => handleClick(i)} />
   );
+
+  // RAV: setState can't be used for initialization, only in effects
+  // because it triggers RERENDERING of component !!!
 
   return (
     <div>
